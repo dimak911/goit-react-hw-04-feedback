735613caa88f173f52e2a9fd78c5cd421881033a
Dmitriy Kniagnitskiy <dimak911@gmail.com>
2022-08-09 22:24:45 +0300

    01

--- src/components/Statistics/Statistics.js
+++ src/components/Statistics/Statistics.js
@@ -1,6 +1,8 @@
 import { StatHeading, StatList } from './Statistics.styled';
 
 export const Statistics = ({ currentState: { good, neutral, bad }, total }) => {
+  const totalReviews = total(good, neutral, bad);
+  const positivePercentage = Math.round((good / totalReviews) * 100);
   return (
     <>
       <StatHeading>Statistics</StatHeading>
@@ -8,8 +10,10 @@ export const Statistics = ({ currentState: { good, neutral, bad }, total }) => {
         <li>Good: {good}</li>
         <li>Neutral: {neutral}</li>
         <li>Bad: {bad}</li>
-        <li>Total: {total(good, neutral, bad)}</li>
-        <li></li>
+        <li>Total: {totalReviews}</li>
+        <li>
+          Positive Feedback: {positivePercentage ? positivePercentage : 0}%
+        </li>
       </StatList>
     </>
   );
